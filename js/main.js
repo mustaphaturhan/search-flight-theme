c59514a1c993ea074111bf8c50239cc9bc8ed4b3
odunluzikkim <odunluzikkim@gmail.com>
2017-03-15 12:29:35 +0300

    updated

--- js/main.js
+++ js/main.js
@@ -8,22 +8,8 @@ $(document).ready(function() {
     $('.btn-cift').on('click', function() {
         $('#donus-tarihi').removeAttr('disabled')
     });
-    $('.btn-for-details').click(function() {
-        $(this).closest('div').find('#get-details').toggle('slideDown');
-        $(this).closest('.flight-list-item').addClass('open-gradient opened');
-        console.log("clicked")
-    })
-    $('.click-and-close').on('click', function() {
-        $('.filter-row').toggle(function() {
-            $('.close-icon').removeClass('fa-times').addClass('fa-filter');
-            $('.list-col').removeClass('col-lg-9').addClass('col-lg-12');
-        }, function() {
-            $('.close-icon').removeClass('fa-filter').addClass('fa-times');
-            $('.list-col').removeClass('col-lg-12').addClass('col-lg-9');
-        });
-    })
 
-    $('#adult-passenger, #student-passenger, #child-passenger, #baby-passenger, #old-passenger, #flight-class').change(function() {
+    $('.spinner-input, #flight-class').change(function() {
         var ap = parseInt($('#adult-passenger').val());
         var sp = parseInt($('#student-passenger').val());
         var cp = parseInt($('#child-passenger').val());
